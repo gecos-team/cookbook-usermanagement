ad535ca8927a87aad80e72eb0af3dc7031939a3c
Juanje Ojeda <jojeda@emergya.es>
2011-12-07 02:56:53 +0100

    Updated the metadata and doc with more accurate info

--- README.md
+++ README.md
@@ -14,11 +14,35 @@ Recipes
 
 The current recipes right now are:
 
-default
--------
-   
-Usage
-=====
+
+Shares
+------
+
+This cookbok install a policy of shares (network shared directories)
+to a specific user of a node.
+
+The cookbook add to (or remove from) the $HOME/.gtk-bookmarks bookmarks
+to be mounted remotely by the node.
+
+This will work just for GNOME desktops, so OS where you can run GNOME.
+
+Attributes
+----------
+
+* `node['shares']['user']` - Name of the user to apply the policies
+* `node['shares']['shares']` - List of shares and actions. Actions can be :add or :remove
+
+Example:
+
+```
+default['shares'][:user] = "juan"
+default['shares'][:shares] = [
+                              {:uri => 'smb://machine_one/path/to/the/directory', :action => "add"},
+                              {:uri => 'smb://remote_host_B/path/Shared',         :action => "add"},
+                              {:uri => 'nfs://server_nfs/isos/',                  :action => "remove"}
+                             ]
+```
+
 
 License and Author
 ==================
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,33 @@
+name              "usermanagement"
+version           "0.1.1"
 maintainer        "rcmorano"
 maintainer_email  "rcmorano@emergya..com"
 license           "Apache 2.0"
 description       "This cookbook provides the ability to change user specific configurations"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
-recipe            "change-background", "set user specific desktop background"
+
+provides          "usermanagement::background"
+provides          "usermanagement::shares"
+
+recipe            "usermanagement::background", "Set user specific desktop background"
+recipe            "usermanagement::shares", "Set the remote resource to be mounted"
 
 %w{ ubuntu debian }.each do |os|
   supports os
 end
+
+attribute 'shares/user',
+  :display_name => "User name",
+  :description  => "Unix name of the user to add or remove the remote resource"
+  :type         => "string",
+  :required     => "required",
+  :recipes      => \[ '::shares' \]
+
+attribute 'shares/shares',
+  :display_name => "Share: remote resource",
+  :description  => "The remote resource's URI and the action to be perfomed: add or remove"
+  :type         => "array",
+  :required     => "required",
+  :recipes      => \[ '::shares' \]
+
+
