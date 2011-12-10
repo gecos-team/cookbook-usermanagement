08605bc7136bde5e9ac8b1975ba28b23b71fed0b
Juanje Ojeda <jojeda@emergya.es>
2011-12-10 05:32:22 +0100

    Added Proxy Socks recipe

--- metadata.rb
+++ metadata.rb
@@ -9,10 +9,12 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 provides          "usermanagement::background"
 provides          "usermanagement::shares"
 provides          "usermanagement::homepage"
+provides          "usermanagement::proxy_socks"
 
 recipe            "usermanagement::background", "Set user specific desktop background"
 recipe            "usermanagement::shares", "Set the remote resource to be mounted"
 recipe            "usermanagement::homepage", "Set the Firefox's homepage"
+recipe            "usermanagement::proxy_socks", "Enable or unable the Proxy Socks"
 
 %w{ ubuntu debian }.each do |os|
   supports os
@@ -45,4 +47,27 @@ attribute 'homepage/homepage',
   :type         => "string",
   :required     => "required",
   :recipes      => [ 'usermanagement::homepage' ]
+
+attribute 'proxy_socks/mode',
+  :display_name => "Mode",
+  :description  => "Enable (manual mode) or disable the proxy socks",
+  :type         => "string",
+  :choice       => [ "manual", "none" ],
+  :required     => "required",
+  :default      => "none",
+  :recipes      => [ 'usermanagement::proxy_socks' ]
+
+attribute 'proxy_socks/host',
+  :display_name => "Host",
+  :description  => "The proxy socks server IP",
+  :type         => "string",
+  :required     => "required",
+  :recipes      => [ 'usermanagement::proxy_socks' ]
+
+attribute 'proxy_socks/port',
+  :display_name => "Port",
+  :description  => "The proxy socks server port",
+  :type         => "string",
+  :required     => "required",
+  :recipes      => [ 'usermanagement::proxy_socks' ]
 
---
+++ recipes/proxy_socks.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: usermanagement
+# Recipe:: proxy_socks
+#
+# Copyright 2011 Junta de Andalucía
+#
+# Author::
+#  * Juanje Ojeda <jojeda@emergya.com>
+#
+# Based on the Roberto C. Morano's applyuserconfs recipe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+users.each do |userdata|
+
+  username = userdata["id"]
+  mode = userdata["proxy_socks"]["mode"]
+
+  usermanagement_desktopsetting "mode" do
+    type "string"
+    value mode
+    schema "org.gnome.system.proxy"
+    username username
+    provider "usermanagement_gsettings"
+    action :set
+  end
+
+  %w{ host port }.each do |key|
+    usermanagement_desktopsetting key do
+      type "string"
+      value userdata["proxy_socks"][key]
+      schema "org.gnome.system.proxy"
+      username username
+      provider "usermanagement_gsettings"
+      action :set
+    end
+  end
+
+end
+
