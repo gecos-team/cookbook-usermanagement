4ab832c2c8988583b453bd5e3ff073d53c60d85b
Juanje Ojeda <jojeda@emergya.es>
2011-12-12 13:38:19 +0100

    Removed the Firefox prefs for the system.
    This is just for already created users.

--- recipes/homepage.rb
+++ recipes/homepage.rb
@@ -23,16 +23,6 @@
 # limitations under the License.
 #
 
-default_prefs = "/usr/share/firefox-firma/defaults/profile/prefs.js"
-
-template default_prefs do
-  source "firefox-prefs.js.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables :homepage => node.homepage
-end
-
 users.each do |userdata|
   username = userdata["id"]
   homepage = userdata["homepage"]["homepage"]
