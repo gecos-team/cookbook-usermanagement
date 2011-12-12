cb3049c6ea5c83eb572368f73cf24159c8b02d2e
Juanje Ojeda <jojeda@emergya.es>
2011-12-12 01:14:14 +0100

    Fixed some headers. License's stuff

--- recipes/shares.rb
+++ recipes/shares.rb
@@ -1,10 +1,23 @@
 #
-# Cookbook Name:: shares
-# Recipe:: default
+# Cookbook Name:: usermanagement
+# Recipe:: shares
 #
-# Copyright 2011, Junta de Andalucía
+# Copyright 2011 Junta de Andalucía
 #
-# GPL v2
+# Author::
+#  * Juanje Ojeda <jojeda@emergya.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 def name_for_uri(uri)
