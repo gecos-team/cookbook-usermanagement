22e9fb294e37f12bf43c26f9795ca0e91df82583
Roberto C. Morano <rcmorano@emergya.com>
2012-01-11 19:28:42 +0100

    changed 'before' attribute kind

--- resources/plain_file.rb
+++ resources/plain_file.rb
@@ -3,7 +3,7 @@ actions :replace, :add, :remove
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :owner, :kind_of => String
 attribute :group, :kind_of => String
-attribute :before, :kind_of => String
+attribute :before, :kind_of => Regexp
 attribute :after, :kind_of => String
 attribute :new_line, :kind_of => String
 attribute :pattern, :kind_of => String
