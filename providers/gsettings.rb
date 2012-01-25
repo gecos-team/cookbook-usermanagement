622a4bd28f801a8b5fd02fb7f4da48caec187dd1
Roberto C. Morano <rcmorano@emergya.com>
2012-01-25 16:52:18 +0100

    providers modified to run 'xvfb-run' with no delay (was 3 seconds)

--- providers/gsettings.rb
+++ providers/gsettings.rb
@@ -1,11 +1,11 @@
 action :set do
   execute "set key" do
-    command "xvfb-run sudo -iu #{new_resource.username} gsettings set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
+    command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
   end
 end
 
 action :unset do
   execute "unset key" do
-    command "xvfb-run sudo -iu #{new_resource.username} gsettings reset #{new_resource.schema} #{new_resource.name}"
+    command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings reset #{new_resource.schema} #{new_resource.name}"
   end
 end
