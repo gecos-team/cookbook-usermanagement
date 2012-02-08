58ee863902401f4e7d510b028cf593e64c0484c8
David Amian <damian@emergya.com>
2012-02-08 23:18:42 +0100

    Added extra check in order to avoid empty array members

--- recipes/shares.rb
+++ recipes/shares.rb
@@ -46,14 +46,16 @@ users.each do |userdata|
 
   # Now the remote resources passed as attributes
   userdata["shares"]["shares"].each do |share|
-    bookmark = name_for_uri(share["uri"])
-
-    usermanagement_plain_file "#{homedir}/.gtk-bookmarks" do
-      pattern share["uri"]
-      new_line "#{share["uri"]} #{bookmark}"
-      owner username
-      group username
-      action share["action"].to_sym if %w{ add remove }.include? share["action"]
+    unless share["uri"].empty?
+      bookmark = name_for_uri(share["uri"])
+  
+      usermanagement_plain_file "#{homedir}/.gtk-bookmarks" do
+        pattern share["uri"]
+        new_line "#{share["uri"]} #{bookmark}"
+        owner username
+        group username
+        action share["action"].to_sym if %w{ add remove }.include? share["action"]
+      end
     end
   end
 
