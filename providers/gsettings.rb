dea423835727dce3dd0451c763e7e72e0f2f4384
Roberto C. Morano <rcmorano@emergya.com>
2012-03-27 12:36:41 +0200

    fixed provider to use correct dbus address in order to see the changes inmediately at the workstation

--- providers/gsettings.rb
+++ providers/gsettings.rb
@@ -10,16 +10,33 @@ def initialize(*args)
     FileUtils.mkdir dconf_cache_dir
     FileUtils.chown_R(new_resource.username, new_resource.username, dconf_cache_dir)  
   end
+
+  dbus_file = Dir["/home/#{new_resource.username}/.dbus/session-bus/*0"].last
+  @dbus_address = open(dbus_file).grep(/^DBUS_SESSION_BUS_ADDRESS=(.*)/){$1}[0]
+        
 end
 
 action :set do
-  execute "set key" do
-    command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
+  dbus_address = @dbus_address
+  unless dbus_address.empty?
+    execute "set key" do
+      command "sudo -iu #{new_resource.username} DBUS_SESSION_BUS_ADDRESS=\"#{dbus_address}\" gsettings set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
+    end
+  else
+    execute "set key" do
+      command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
+    end
   end
 end
 
 action :unset do
-  execute "unset key" do
-    command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings reset #{new_resource.schema} #{new_resource.name}"
+  unless dbus_address.empty?
+    execute "unset key" do
+      command "sudo -iu #{new_resource.username} DBUS_SESSION_BUS_ADDRESS=\"#{@dbus_address}\" gsettings reset #{new_resource.schema} #{new_resource.name}"
+    end
+  else
+    execute "unset key" do
+      command "xvfb-run -w 0 sudo -iu #{new_resource.username} gsettings reset #{new_resource.schema} #{new_resource.name}"
+    end
   end
 end
